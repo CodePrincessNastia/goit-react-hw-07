39a60f7987276f5b3dab1206e6364a1aa2bf9993
CodePrincessNastia <145288463+CodePrincessNastia@users.noreply.github.com>
2024-05-21 16:18:27 +0300

    add HW

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from './App.module.css';
+import ContactForm from "../ContactForm/ContactForm"
+import SearchBox from "../SearchBox/SearchBox"
+import ContactList from "../ContactList/ContactList"
+import Loader from '../Loader/Loader';
+import Error from '../Error/Error';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectLoading } from '../../redux/contactsSlice';
+import { useEffect } from 'react';
+import { fetchContacts } from '../../redux/contactsOps';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1 className={css.header}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      {loading && <Loader>Loading tasks, please wait</Loader>}
+      {error && <Error>Error message</Error>}
+      <ContactList />
+    </div>
+  );
 }
-
-export default App
